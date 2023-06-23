9e12c1e2663c830cf434d833a3654fb6264c5fc7
damiandev <damikalicki@gmail.com>
2023-06-23 22:04:11 +0200

    Project initialization

---
+++ microapp-customers/src/test/java/com/damiandev/microapp/microappcustomers/MicroappCustomersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.damiandev.microapp.microappcustomers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroappCustomersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
